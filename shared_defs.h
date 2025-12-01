cbe7a48c0dc14ba6ceb034d0fc68dc0e1e4cf1bd
Danilo Bukvic <dan.bukvic@icloud.com>
2025-11-30 19:00:05 -0500

    added semaphores to constants

--- shared_defs.h
+++ shared_defs.h
@@ -5,9 +5,23 @@
 
 // Constants
 #define NUM_EXERCISES 5
-#define MAX_RUBRIC_LINE_LEN 256
+#define SEM_KEY 54321 // Semaphor key
 #define MAX_EXAM_FILES 21 // 20 regular exams + 1 (9999) termination file
 #define SHM_KEY 12345 // Unique key for shared memory segment
+
+
+ // semaphore indices
+
+enum{
+    RUBRIC_WRITE_SEM, // Index 0: Mutex to protect writing to the rubric file
+    EXAM_LOAD_SEM,    // Index 1: Mutex to protect loading the next exam into shared memory
+    Q1_MARK_SEM,      // Index 2: Mutex for marking Question 1
+    Q2_MARK_SEM,      // Index 3: Mutex for marking Question 2
+    Q3_MARK_SEM,      // Index 4: Mutex for marking Question 3
+    Q4_MARK_SEM,      // Index 5: Mutex for marking Question 4
+    Q5_MARK_SEM,      // Index 6: Mutex for marking Question 5
+    NUM_SEMAPHORES    // Total count: 7
+};
 
 // --- Rubric Structure ---
 // Represents one line in the rubric file (e.g., "1, A")
@@ -34,8 +48,7 @@ typedef struct {
     int next_exam_index;      // Index/ID of the next exam file to load (e.g., 1 to 21)
     int ta_count;             // Total number of TAs (processes) created
     int is_terminated;        // Flag to signal all TAs to stop (e.g., when 9999 is reached)
-
-    // Note: Semaphores would be added here or created separately for Part 2.b
+    int sem_id; 
 } SharedData;
 
 #endif // SHARED_DEFS_H
